e6d001e6a6a469b800d725fb47da74326d516c67
ArslanGhaffar <74699371+ArslanGhaffar@users.noreply.github.com>
2024-06-05 01:27:28 +0500

    Add files via upload
    Develop a Python-based calculator that can perform basic arithmetic operations through a
    command-line interface

---
+++ Calculator by Arslan Ghaffar.py
@@ -0,0 +1,45 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')
+if __name__ == '__main__':
+    print_hi('I am your calculator, choose below & do let me know how you like me to proceed')
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero is not allowed"
+    return x / y
+
+print("Calculator by Arslan Ghaffar")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter your choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+else:
+    print("Invalid choice")
+    
+
